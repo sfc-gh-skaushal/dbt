ed78d513a9e3b5166d249e3cd5a8dcd8f5e7e7e0
Siddharth Kaushal <siddharth.kaushal@snowflake.com>
2025-09-30 22:36:15 +0200

    Update dbt_projects.sql

--- tasty_bytes_dbt_demo/setup/dbt_projects.sql
+++ tasty_bytes_dbt_demo/setup/dbt_projects.sql
@@ -1,23 +1,28 @@
-  use role sysadmin;
+TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES  use role sysadmin;
   use database TASTY_BYTES_DBT_DB;
-  use schema public;
+  use schema raw;
 
 
- CREATE DBT PROJECT TASTY_BYTES_DBT_DB.public.tastybytes_truck_demo
+ CREATE DBT PROJECT TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES
   FROM 'snow://workspace/USER$SKAUSHAL.PUBLIC."tasty_truck_roll"/versions/live';
 
- 
+
 
 show dbt projects;
 
-  EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.public.tastybytes_truck_demo
+EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES
+args='deps --target dev' external_access_integrations = (DBT_ACCESS_INTEGRATION);
+
+  EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES
   ARGS = 'compile  --target dev';
 
-    EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.public.tastybytes_truck_demo
+    EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES
   ARGS = 'test  --target dev';
 
-      EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.public.tastybytes_truck_demo
+      EXECUTE DBT PROJECT TASTY_BYTES_DBT_DB.RAW.DBT_TRUCK_SALES
   ARGS = 'run  --target dev';
+
+  
 
   
 
